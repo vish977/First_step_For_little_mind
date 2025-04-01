65b5c567683be64e34c10bcc06aaf2948e5c3b41
akankshavns <makanksha858@gmail.com>
2025-04-01 11:10:43 +0530

    Add audio in the learning.

--- First_step_For_little_mind/First_step_For_little_mind.Shared/wwwroot/js/A.js
+++ First_step_For_little_mind/First_step_For_little_mind.Shared/wwwroot/js/A.js
@@ -1,5 +1,11 @@
-﻿//window.addEventListener("load", () => {
-//    setTimeout(() => {
-//        document.getElementById("Words").classList.add("expanded");
-//    }, 500);
-//});
+﻿document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOM is fully loaded!");
+    var canvas = document.getElementById("Tracing");
+    var ctx = canvas.getContext("2d");
+
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "White";
+    ctx.fillText("A", 50, 100);
+
+
+});
