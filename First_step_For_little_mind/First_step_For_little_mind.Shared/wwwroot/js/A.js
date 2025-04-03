cc9a8e1b6dd1fc433fff5f13c4e7bdfda135e8e9
akankshavns <makanksha858@gmail.com>
2025-04-03 19:29:56 +0530

    change the image of alphabets chart and add new page vowels,

--- First_step_For_little_mind/First_step_For_little_mind.Shared/wwwroot/js/A.js
+++ First_step_For_little_mind/First_step_For_little_mind.Shared/wwwroot/js/A.js
@@ -1,11 +1,30 @@
-﻿document.addEventListener("DOMContentLoaded", function () {
-    console.log("DOM is fully loaded!");
-    var canvas = document.getElementById("Tracing");
-    var ctx = canvas.getContext("2d");
+﻿
+    //var canvas = document.getElementById("Tracing");
+   
+    ////const ctx = canvas.getContext('2d');
 
-    ctx.font = "30px Arial";
-    ctx.fillStyle = "White";
-    ctx.fillText("A", 50, 100);
+    //function drawRoundedA(x, y, width, height, radius) {
+    //    ctx.beginPath();
 
+    //    // Left side of the A
+    //    ctx.moveTo(x + radius, y + height);
+    //    ctx.lineTo(x, y + radius);
+    //    ctx.arc(x + radius, y + radius, radius, Math.PI, 1.5 * Math.PI);
+    //    ctx.lineTo(x + width / 2 - radius, y);
 
-});
+    //    // Top of the A
+    //    ctx.arc(x + width / 2, y + radius, radius, 1.5 * Math.PI, 1.75 * Math.PI); // Adjust arc angle for rounded top
+    //    ctx.arc(x + width / 2, y + radius, radius, 1.75 * Math.PI, 2 * Math.PI);
+
+    //    // Right side of the A
+    //    ctx.lineTo(x + width, y + radius);
+    //    ctx.arc(x + width - radius, y + radius, radius, 0, 0.5 * Math.PI);
+    //    ctx.lineTo(x + width - radius, y + height);
+
+    //    ctx.closePath();
+    //    ctx.fillStyle = 'white'; // Fill the A with white
+    //    ctx.fill();
+    //}
+
+    //// Draw the A
+    //drawRoundedA(20, 20, 160, 260, 20); // Adjust parameters as needed
