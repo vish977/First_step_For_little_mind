c9dc0a9795bae9a4944253a624943153b52ee11f
akankshavns <makanksha858@gmail.com>
2025-04-08 14:57:41 +0530

    Attach three and four letter word for learning.

---
+++ First_step_For_little_mind/First_step_For_little_mind.Shared/List/RequiredList.cs
@@ -0,0 +1,37 @@
+﻿namespace First_step_For_little_mind.Shared.List;
+
+public static class RequiredList
+{
+    public static List<string> AaKiMatra => new()
+    {
+        "आम", "राजा", "काला", "माला", "छाता", "भाला", "दादा", "बाघ", "तारा", "चाचा"
+    };
+
+    public static List<string> IKiMatra => new()
+    {
+        "किताब", "गिरा", "मिट्टी", "पिता", "निकल", "बिल्ली", "चिड़िया", "पिन", "लिपि", "सितार"
+    };
+
+    public static List<string> EeKiMatra => new()
+    {
+        "चींटी", "सीता", "मीठा", "पानी", "रीढ़", "चाबी", "झील", "कुर्सी", "नीति", "ताली"
+    };
+
+    public static List<string> FourLetterWords => new()
+    {
+        "Book", "Ball", "Fish", "Goat", "Hand", "Jump", "Kite", "Lamb", "Leaf", "Lion",
+        "Milk", "Moon", "Nest", "Nose", "Oven", "Play", "Quiz", "Rain", "Ring", "Rock",
+        "Ship", "Shoe", "Star", "Tail", "Tent", "Time", "Toes", "Tree", "Trip", "Turn",
+        "Vase", "View", "Wall", "Wave", "Wind", "Wood", "Worm", "Yarn", "Yell", "Zero",
+        "Zinc", "Zone", "Zoom", "Peel", "Seed", "Soil", "Frog", "Crab", "Deer", "Cake"
+    };
+
+    public static List<string> ThreeLetterWords => new()
+    {
+        "Arm", "Bay", "Bed", "Big", "Bin", "Bit", "Box", "Bug", "Bun", "Bus",
+        "Can", "Car", "Cod", "Cub", "Cut", "Den", "Dig", "Dip", "Dry", "Dye",
+        "Eat", "End", "Far", "Fat", "Fig", "Fix", "Fun", "Get", "Gun", "Guy",
+        "Hop", "Hut", "Jaw", "Joy", "Kid", "Lap", "Leg", "Log", "Man", "Mat",
+        "Mug", "Nap", "Net", "Nut", "Pet", "Pop", "Rug", "Run", "Sun", "Tip"
+    };
+}
