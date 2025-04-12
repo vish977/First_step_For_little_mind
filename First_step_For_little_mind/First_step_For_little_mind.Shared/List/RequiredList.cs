a113b1ccfa1d9b74bcbd33d4a5736a5b0a8596ee
akankshavns <makanksha858@gmail.com>
2025-04-12 22:05:30 +0530

    Add  images of 3 and 4 letter words to make learning easy for kids.

--- First_step_For_little_mind/First_step_For_little_mind.Shared/List/RequiredList.cs
+++ First_step_For_little_mind/First_step_For_little_mind.Shared/List/RequiredList.cs
@@ -19,26 +19,20 @@ public static class RequiredList
 
     public static List<string> FourLetterWords => new()
     {
-        "Book", "Ball", "Fish", "Goat", "Hand", "Jump", "Kite", "Lamb", "Leaf", "Lion",
-        "Milk", "Moon", "Nest", "Nose", "Oven", "Play", "Quiz", "Rain", "Ring", "Rock",
-        "Ship", "Shoe", "Star", "Tail", "Tent", "Time", "Toes", "Tree", "Trip", "Turn",
-        "Vase", "View", "Wall", "Wave", "Wind", "Wood", "Worm", "Yarn", "Yell", "Zero",
-        "Zinc", "Zone", "Zoom", "Peel", "Seed", "Soil", "Frog", "Crab", "Deer", "Cake"
+        "Cook", "Phone", "Rock", "Boat", "Hand", "Jump", "Kite", "Tall", "List", "Bell",
+        "Milk", "Sand", "Cake", "Coin", "Cube", "Crab", "Face", "Rain", "Ring", "Bird",
+        "Card", "Shoe", "Star", "Tail", "Tent", "Time", "Hill", "Tree", "Corn", "Turn",
+        "Deep", "Desk", "Wall", "Wave", "Wind", "Wood", "Worm", "Bear", "Body", "Cone",
+        "West", "Bone", "Gold", "Peel", "Seed", "Soil", "Door", "East", "Deer", "Hair"
     };
 
     public static List<string> ThreeLetterWords => new()
     {
-        "Arm", "Bay", "Bed", "Big", "Bin", "Bit", "Box", "Bug", "Bun", "Bus",
-        "Can", "Car", "Cod", "Cub", "Cut", "Den", "Dig", "Dip", "Dry", "Dye",
-        "Eat", "End", "Far", "Fat", "Fig", "Fix", "Fun", "Get", "Gun", "Guy",
-        "Hop", "Hut", "Jaw", "Joy", "Kid", "Lap", "Leg", "Log", "Man", "Mat",
-        "Mug", "Nap", "Net", "Nut", "Pet", "Pop", "Rug", "Run", "Sun", "Tip"
-    };
-
-    public static List<string> Poems => new()
-    {
-        "TwinkleTwinkle", "RainRain"
-
+        "Arm", "Bay", "Bed", "Bud", "Bin", "Ape", "Box", "Bug", "Bun", "Bus",
+        "Can", "Car", "Tap", "Cub", "Cut", "Den", "Dig", "Dip", "Dry", "Dye",
+        "Eat", "Fog", "Far", "Fat", "Wet", "Pig", "Sky", "Pin", "Gun", "pen",
+        "Dot", "Hut", "Jaw", "Pot", "Kid", "Rat", "Leg", "Map", "Man", "Mat",
+        "Mug", "Toy", "Net", "Nut", "Pet", "Son", "Rug", "Run", "Sun", "Bee"
     };
     
 }
