03419d450f7aef6f17bfba7d395099508a08ea92
akankshavns <makanksha858@gmail.com>
2025-04-14 22:21:53 +0530

    create a well practice paper set.

--- First_step_For_little_mind/First_step_For_little_mind.Shared/wwwroot/js/PracticeSheet.js
+++ First_step_For_little_mind/First_step_For_little_mind.Shared/wwwroot/js/PracticeSheet.js
@@ -1,48 +1,4 @@
-﻿//window.highlightItem = function (id) {
-//    document.querySelectorAll(".item").forEach(el => {
-//        el.style.border = "2px solid #333";
-//    });
-//    const el = document.querySelector(`#columnA .item[data-id='${id}']`);
-//    if (el) el.style.border = "2px solid blue";
-//};
-
-//window.drawLineBetweenElements = function (aId, bId) {
-//    const canvas = document.getElementById("connectorCanvas");
-//    const ctx = canvas.getContext("2d");
-
-//    const elA = document.querySelector(`#columnA .item[data-id='${aId}']`);
-//    const elB = document.querySelector(`#columnB .item[data-id='${bId}']`);
-
-//    if (!elA || !elB) return;
-
-//    const rect1 = elA.getBoundingClientRect();
-//    const rect2 = elB.getBoundingClientRect();
-//    const canvasRect = canvas.getBoundingClientRect();
-
-//    const x1 = rect1.right - canvasRect.left;
-//    const y1 = rect1.top + rect1.height / 2 - canvasRect.top;
-//    const x2 = rect2.left - canvasRect.left;
-//    const y2 = rect2.top + rect2.height / 2 - canvasRect.top;
-
-//    ctx.strokeStyle = "red";
-//    ctx.lineWidth = 2;
-//    ctx.beginPath();
-//    ctx.moveTo(x1, y1);
-//    ctx.lineTo(x2, y2);
-//    ctx.stroke();
-//};
-
-//window.redrawConnections = function (connections) {
-//    const canvas = document.getElementById("connectorCanvas");
-//    const ctx = canvas.getContext("2d");
-//    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-//    connections.forEach(conn => {
-//        window.drawLineBetweenElements(conn.A, conn.B);
-//    });
-//};
-
-window.redrawConnections = (connections) => {
+﻿window.redrawConnections = (connections) => {
     const canvas = document.getElementById("connectorCanvas");
     const ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas before redrawing
